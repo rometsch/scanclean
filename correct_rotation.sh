3542afe17b1f9f71064761268e814487240e67d3
Thomas Rometsch <thomas.rometsch@gmail.com>
2019-09-12 17:29:15 +0200

    use command line argument for output file

--- correct_rotation.sh
+++ correct_rotation.sh
@@ -4,4 +4,11 @@ echo $SCRIPTDIR
 GETROTATION="$SCRIPTDIR/detect-rotation.py -c"
 CORRECTIONANGLE="$($GETROTATION $1)"
 echo "$(basename $1) : $CORRECTIONANGLE"
-convert -rotate $CORRECTIONANGLE "$1" "rot-$1"
+
+if [[ "$#" > 1 ]]; then
+	OUTPATH="$2"
+else
+	OUTPATH="rot-$1"
+fi
+
+convert -rotate $CORRECTIONANGLE "$1" "$OUTPATH"
